6ccc762dc654fcb67822dba8997eb1b55d6ff5de
Joe-Wehbe <joewehbe@yahoo.com>
2024-10-15 19:02:00 +0300

    01-starting-project (S2-27) <> Worked on the user component

--- 01-starting-project/src/app/app.component.ts
+++ 01-starting-project/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from "./header.component";
+import { HeaderComponent } from "./header/header.component";
+import { UserComponent } from "./user/user.component";
 
 // decorator (adds additional metadata to the class)
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, UserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
