2b758955491bef730b1b652cd0f387d08b6fe5aa
Joe-Wehbe <joewehbe@yahoo.com>
2024-10-24 13:08:52 +0300

    01-starting-project (S2-43) <> Outputted users dynamically

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -1,7 +1,18 @@
-import { Component } from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users';
+import { Component, EventEmitter, Input, Output} from '@angular/core';
 
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+// TYPE ALIAS
+// type User = {
+//   id: string;
+//   avatar: string;
+//   name: string;
+// }
+
+// INTERFACE (another way of defining an object type)
+interface User {
+  id: string;
+  avatar: string;
+  name: string;
+}
 
 @Component({
   selector: 'app-user',
@@ -10,15 +21,46 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex];
 
-  // getter
+  /**************** INPUT WITH DECORATOR ****************/
+
+  // @Input are used to add properties to the component
+  @Input({ required: true }) user!: User;  // '!' indicates that the variable will be set to some value
+  // @Input({ required: true }) avatar!: string;
+  // @Input({ required: true }) name!: string;
+
+  // This is a user object type, we can outsource it with a type alias
+  // @Input({ required: true }) user!: {
+  //   id: string;
+  //   avatar: string;
+  //   name: string;
+  // }
+
+  // Getter for the image path
   get imagePath() {
-    return 'assets/users/' + this.selectedUser.avatar;
+    return 'assets/users/' + this.user.avatar;
   }
 
+  /***************** INPUT WITH SIGNALS *****************/
+
+  // avatar = input.required<string>() // you can pass an initial value
+  // name = input.required<string>()
+
+  // imagePath = computed(() => {
+  //   return 'assets/users/' + this.avatar;
+  // })
+
+  /**************** OUTPUT WITH DECORATOR ****************/
+
+  // @Output are used to emit events when the component is used
+  @Output() select = new EventEmitter();
+
+  /************* ALTERNATIVE OUTPUT (MODERN) *************/
+  // select = output<string>(); // EventEmitter will automatically be created
+
+
+  // This function emits an event (sending the user id) once a component is selected
   onSelectUser() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser = DUMMY_USERS[randomIndex];
+    this.select.emit(this.user.id);
   }
 }
