06ecbbe384d890165d0506ddc8264718e2abee68
Joe-Wehbe <joewehbe@yahoo.com>
2024-11-08 20:43:52 +0200

    02-investment-calculator-signals-services (S4-87) <> Implemented services

--- 02-investment-calculator/src/app/investment-results/investment-results.component.ts
+++ 02-investment-calculator/src/app/investment-results/investment-results.component.ts
@@ -1,5 +1,6 @@
-import { Component, input } from '@angular/core';
+import { Component, computed } from '@angular/core';
 import { CurrencyPipe } from '@angular/common';
+import { InvestmentService } from '../investment.service';
 
 @Component({
   selector: 'app-investment-results',
@@ -9,12 +10,15 @@ import { CurrencyPipe } from '@angular/common';
   styleUrl: './investment-results.component.css'
 })
 export class InvestmentResultsComponent {
-  results = input<{
-    year: number,
-    interest: number,
-    valueEndOfYear: number,
-    annualInvestment: number,
-    totalInterest: number,
-    totalAmountInvested: number,
-  }[]>()
+  constructor(private investmentService: InvestmentService) {}
+
+  // get results() {
+  //   return this.investmentService.resultData;
+  // }
+
+  // Same as commented code above, but makes the signal read only
+  results = computed(() => this.investmentService.resultData())
+
+  // Same as commented code above (alternative to computed)
+  // results = this.investmentService.resultData.asReadonly();
 }
--- 02-investment-calculator/src/app/user-input/user-input.component.ts
+++ 02-investment-calculator/src/app/user-input/user-input.component.ts
@@ -1,7 +1,6 @@
-import { Component, output, signal } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms'
-import { InvestmentInput } from '../investment-input.model';
-import { InvestmentResultsComponent } from '../investment-results/investment-results.component';
+import { InvestmentService } from '../investment.service';
 
 @Component({
   selector: 'app-user-input',
@@ -11,14 +10,15 @@ import { InvestmentResultsComponent } from '../investment-results/investment-res
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
-  calculate = output<InvestmentInput>();
   enteredInitialInvestment = signal('0');
   enteredAnnualInvestment = signal('0');
   enteredExpectedReturn = signal('5');
   enteredDuration = signal('10');
 
+  constructor(private investmentService: InvestmentService) {}
+
   onSubmit() {
-    this.calculate.emit({
+    this.investmentService.calculateInvestmentResults({
       initialInvestment: +this.enteredInitialInvestment(),
       annualInvestment: +this.enteredAnnualInvestment(),
       expectedReturn: +this.enteredExpectedReturn(),
