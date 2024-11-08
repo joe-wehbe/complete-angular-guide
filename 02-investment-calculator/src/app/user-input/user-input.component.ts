94a415d472390a02d126836ee00f2eda7327829d
Joe-Wehbe <joewehbe@yahoo.com>
2024-11-08 19:43:18 +0200

    02-investment-calculator-signals (S4-85) <> Implemented signlas across the application

--- 02-investment-calculator/src/app/user-input/user-input.component.ts
+++ 02-investment-calculator/src/app/user-input/user-input.component.ts
@@ -1,6 +1,7 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, output, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms'
 import { InvestmentInput } from '../investment-input.model';
+import { InvestmentResultsComponent } from '../investment-results/investment-results.component';
 
 @Component({
   selector: 'app-user-input',
@@ -10,20 +11,18 @@ import { InvestmentInput } from '../investment-input.model';
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
-
-  @Output() calculate = new EventEmitter<InvestmentInput>()
-
-  enteredInitialInvestment = '0';
-  enteredAnnualInvestment = '0';
-  enteredExpectedReturn = '5';
-  enteredDuration = '10';
+  calculate = output<InvestmentInput>();
+  enteredInitialInvestment = signal('0');
+  enteredAnnualInvestment = signal('0');
+  enteredExpectedReturn = signal('5');
+  enteredDuration = signal('10');
 
   onSubmit() {
     this.calculate.emit({
-      initialInvestment: +this.enteredInitialInvestment,
-      annualInvestment: +this.enteredAnnualInvestment,
-      expectedReturn: +this.enteredExpectedReturn,
-      duration: +this.enteredDuration
+      initialInvestment: +this.enteredInitialInvestment(),
+      annualInvestment: +this.enteredAnnualInvestment(),
+      expectedReturn: +this.enteredExpectedReturn(),
+      duration: +this.enteredDuration(),
     });
   }
 }
