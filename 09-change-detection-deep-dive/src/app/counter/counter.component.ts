87ce1eb058a4f8d6e99bcaffb3a0e1f720216d2a
Joe-Wehbe <joewehbe@yahoo.com>
2025-01-03 17:09:39 +0200

    09-change-detection-deep-dive (S10-203) <> Uploaded the end of the project

--- 09-change-detection-deep-dive/src/app/counter/counter.component.ts
+++ 09-change-detection-deep-dive/src/app/counter/counter.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, signal } from '@angular/core';
 
 import { InfoMessageComponent } from '../info-message/info-message.component';
 
@@ -8,13 +8,24 @@ import { InfoMessageComponent } from '../info-message/info-message.component';
   templateUrl: './counter.component.html',
   styleUrl: './counter.component.css',
   imports: [InfoMessageComponent],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class CounterComponent {
+export class CounterComponent implements OnInit{
   count = signal(0);
 
   get debugOutput() {
     console.log('[Counter] "debugOutput" binding re-evaluated.');
     return 'Counter Component Debug Output';
+  }
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.count.set(0);
+    }, 4000);
+
+      setTimeout(() => {
+        console.log('Timer expired!');
+      }, 5000);
   }
 
   onDecrement() {
