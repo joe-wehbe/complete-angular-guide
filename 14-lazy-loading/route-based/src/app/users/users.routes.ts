634fe21ced6a650c52edaad5af50d0aa018ff669
Joe-Wehbe <joewehbe@yahoo.com>
2025-02-09 20:59:28 +0200

    14-lazy-loading (S15-307) <> Uploaded project

---
+++ 14-lazy-loading/route-based/src/app/users/users.routes.ts
@@ -0,0 +1,35 @@
+import { ResolveFn, Routes } from '@angular/router';
+import { NewTaskComponent, canLeaveEditPage } from '../tasks/new-task/new-task.component';
+import { Task } from '../tasks/task/task.model';
+import { inject } from '@angular/core';
+import { TasksService } from '../tasks/tasks.service';
+import { TasksComponent, resolveUserTasks } from '../tasks/tasks.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    providers:[TasksService],
+    children: [
+      {
+        path: '',
+        redirectTo: 'tasks',
+        pathMatch: 'full',
+      },
+      {
+        path: 'tasks', // <your-domain>/users/<uid>/tasks
+        component: TasksComponent,
+        // loadComponent: () => import('../tasks/tasks.component').then(mod => mod.TasksComponent),
+        runGuardsAndResolvers: 'always',
+        resolve: {
+          userTasks: resolveUserTasks,
+        },
+      },
+      {
+        path: 'tasks/new',
+        component: NewTaskComponent,
+        canDeactivate: [canLeaveEditPage]
+      },
+    ]
+  }
+
+];
